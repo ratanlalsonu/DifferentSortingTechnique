004ac1db66a9d2a2ef342689ee3ae350546ae680
Ratan Lal Gaund <156018467+ratanlalsonu@users.noreply.github.com>
2025-08-20 23:22:25 +0530

    Update quicksort.c

---
+++ different sorting technique/quicksort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int stack[n];
+    int top = -1;
+    stack[++top] = 0;
+    stack[++top] = n - 1;
+    while (top >= 0) {
+        int high = stack[top--];
+        int low = stack[top--];
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        int pi = i + 1;
+        if (pi - 1 > low) {
+            stack[++top] = low;
+            stack[++top] = pi - 1;
+        }
+        if (pi + 1 < high) {
+            stack[++top] = pi + 1;
+            stack[++top] = high;
+        }
+    }
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
